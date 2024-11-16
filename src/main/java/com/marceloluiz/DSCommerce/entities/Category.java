68204ceb9ace802efc38d73235ed2d08f77d5dfe
MaarceloLuiz <marceloluizgpereira@gmail.com>
2024-11-16 23:01:58 +0000

    Category and Product updated

--- src/main/java/com/marceloluiz/DSCommerce/entities/Category.java
+++ src/main/java/com/marceloluiz/DSCommerce/entities/Category.java
@@ -1,4 +1,22 @@
 package com.marceloluiz.DSCommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Builder
+@Data
+@Entity
+@Table(name = "tb_category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 }
--- src/main/java/com/marceloluiz/DSCommerce/entities/Product.java
+++ src/main/java/com/marceloluiz/DSCommerce/entities/Product.java
@@ -1,4 +1,29 @@
 package com.marceloluiz.DSCommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Builder
+@Data
+@Entity
+@Table(name = "tb_product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 }
