1910757ff721328713505e68287e5da1d7bf7976
MaarceloLuiz <marceloluizgpereira@gmail.com>
2025-01-06 19:37:25 +0000

    feat: add search Products by name

--- src/main/java/com/marceloluiz/DSCommerce/repositories/ProductRepository.java
+++ src/main/java/com/marceloluiz/DSCommerce/repositories/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.marceloluiz.DSCommerce.repositories;
 
 import com.marceloluiz.DSCommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj " +
+            "WHERE UPPER(obj.name) " +
+            "LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable); //search for a product using the search bar
 }
